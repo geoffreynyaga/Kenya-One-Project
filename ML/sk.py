bcc66202abacdd0429e52631cc3c1974800e975b
geoffreynyaga <geoffreynyagak@gmail.com>
2017-06-02 09:24:43 +0300

    all

---
+++ ML/sk.py
@@ -0,0 +1,11 @@
+from sklearn import tree
+
+features =  [  [140,1], [130,1], [150,0], [170,0] ]
+
+labels = [ 0,0,1,1]
+
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features,labels)
+
+print(clf.predict([[120,0]]))
+
