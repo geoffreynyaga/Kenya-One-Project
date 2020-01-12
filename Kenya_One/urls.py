52b40da27561a9b51cb55ea2eeb0970909127e08
geoffreynyaga <geoffreynyagak@gmail.com>
2020-01-12 15:35:05 +0300

    example API View for image conversion

--- Kenya_One/urls.py
+++ Kenya_One/urls.py
@@ -14,6 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls import url
+from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls)]
+urlpatterns = [path("admin/", admin.site.urls),
+    url(r"^api/accounts/", include("accounts.api.urls")),
+]
