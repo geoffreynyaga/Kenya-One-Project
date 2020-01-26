47dd0f8f70bf3a81b7096e2133c82612c6db5dba
geoffreynyaga <geoffreynyagak@gmail.com>
2020-01-26 16:20:21 +0300

    REPLACED: old matplotlib image component with plotly graph

--- Frontend-Electron/src/InitialSizing.js
+++ Frontend-Electron/src/InitialSizing.js
@@ -33,12 +33,18 @@
  */
 
 import React, { Component } from "react";
-import "./App.css";
+import { Slider } from "shards-react";
+// import Plot from "react-plotly.js";
+
+import Plotly from "plotly.js-basic-dist";
+
+import createPlotlyComponent from "react-plotly.js/factory";
+const Plot = createPlotlyComponent(Plotly);
 
 // type Group = { status: string; image: string };
 
 // type AppState = {
-//   groups: Group | null;
+//   data: Group | null;
 //   isLoading: boolean;
 // };
 // type AppProps = {};
@@ -49,67 +55,170 @@ class InitialSizing extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      groups: null,
-      isLoading: false
+      data: null,
+      isLoading: false,
+      valueX: [2000, 6000]
     };
   }
-  fetchMTOWPlot = () => {
-    let thisComp = this;
-    fetch("http://localhost:8000/api/accounts/example/")
-      .then(response => {
-        return response.json();
-      })
-      .then(myJson => {
-        console.log(myJson);
-        thisComp.setState({
-          groups: myJson,
-          isLoading: false
-        });
-      })
-      .catch(error => {
-        console.log(error, "error in fetchMTOWPlot");
-      });
-  };
 
-  componentDidMount() {
+  handleSlideX = e => {
+    let thisComp = this;
     this.setState(
       {
-        isLoading: true
+        valueX: [parseFloat(e[0]), parseFloat(e[1])]
       },
-      this.fetchMTOWPlot()
+      thisComp.handleAxisRangeChange([parseFloat(e[0]), parseFloat(e[1])])
     );
-  }
+  };
+
+  handleAxisRangeChange = data => {
+    console.log(
+      data,
+      "handleAxisRangeChange have been called and will pass the above data"
+    );
+    this.props.getAxisChangeData(data);
+  };
 
   render() {
+    console.log("+++++++++ InitialSizing +++++++++++");
+    console.log(this.props, "props");
+    const { wtoGuess } = this.props.data ? this.props.data : [];
+    const { wtoYaxisRaymer } = this.props.data ? this.props.data : [];
+    const { wtoYaxisGud } = this.props.data ? this.props.data : [];
+    const { wtoYaxisRoskam } = this.props.data ? this.props.data : [];
+    const { wtoYaxisSadraey } = this.props.data ? this.props.data : [];
+
+    const { raymerIntersect } = this.props.data ? this.props.data : [];
+    const { gudmundssonIntersect } = this.props.data ? this.props.data : [];
+    const { roskamIntersect } = this.props.data ? this.props.data : [];
+    const { sadraeyIntersect } = this.props.data ? this.props.data : [];
+
+    const { raymer_idx } = this.props.data ? this.props.data : [];
+    const { gudmundsson_idx } = this.props.data ? this.props.data : [];
+    const { roskam_idx } = this.props.data ? this.props.data : [];
+    const { sadraey_idx } = this.props.data ? this.props.data : [];
+
     return (
       <div>
-        <h1>Groups</h1>
-        {!this.state.isLoading ? (
-          <button
-            onClick={() => {
-              this.setState(
-                {
-                  isLoading: true
-                },
-                this.fetchMTOWPlot()
-              );
-            }}
-          >
-            Fetch Groups
-          </button>
-        ) : (
-          ""
-        )}
-
-        {/* <p>{JSON.stringify(this.state.groups)}</p> */}
+        <h1>data</h1>
 
         {this.state.isLoading ? <p>Calculating....</p> : ""}
 
-        {this.state.groups !== null && !this.state.isLoading ? (
-          <img
-            src={`data:image/png;base64,${this.state.groups.image}`}
-            alt="mtow-plot"
-          />
+        {this.props.data !== null && !this.props.isLoading ? (
+          <div>
+            {/* <img
+              src={`data:image/png;base64,${this.state.data.image}`}
+              alt="mtow-plot"
+            /> */}
+
+            <p>ValueX: {JSON.stringify(this.state.valueX)}</p>
+
+            <Slider
+              connect
+              pips={{ mode: "steps", stepped: true, density: 3 }}
+              onSlide={this.handleSlideX}
+              start={this.state.valueX}
+              range={{ min: 1, max: 15000 }}
+            />
+
+            <Plot
+              data={[
+                {
+                  x: wtoGuess,
+                  y: wtoGuess,
+                  // type: "scatter",
+                  mode: "lines",
+                  line: { color: "#2D7FB9", size: 2 },
+                  name: "Wto Guess"
+                },
+                {
+                  x: wtoGuess,
+                  y: wtoYaxisRaymer,
+                  type: "scatter",
+                  mode: "lines+markers",
+                  marker: { color: "#FF7F0E", size: 3 },
+                  line: { color: "#FF7F0E", size: 1 },
+
+                  name: "Raymer"
+                },
+                {
+                  x: wtoGuess,
+                  y: wtoYaxisGud,
+                  type: "scatter",
+                  mode: "lines+markers",
+                  marker: { color: "#2CA02C", size: 3 },
+                  name: "Gudmundsson"
+                },
+                {
+                  x: wtoGuess,
+                  y: wtoYaxisRoskam,
+                  type: "scatter",
+                  mode: "lines+markers",
+                  marker: { color: "#D62728", size: 3 },
+                  name: "Roskam"
+                },
+                {
+                  x: wtoGuess,
+                  y: wtoYaxisSadraey,
+                  type: "scatter",
+                  mode: "lines+markers",
+                  marker: { color: "#A57FC8", size: 3 },
+                  name: "Sadraey"
+                },
+                {
+                  x: raymer_idx,
+                  y: raymerIntersect,
+                  type: "scatter",
+                  mode: "markers",
+                  marker: { color: "red", size: 10 },
+                  name: `Raymer MTOW (${Math.floor(raymerIntersect)} lbs)`
+                },
+                {
+                  x: gudmundsson_idx,
+                  y: gudmundssonIntersect,
+                  type: "scatter",
+                  mode: "markers",
+                  marker: { color: "#2CA02C", size: 10 },
+                  name: `Gudmundsson MTOW (${Math.floor(
+                    gudmundssonIntersect
+                  )} lbs)`
+                },
+                {
+                  x: roskam_idx,
+                  y: roskamIntersect,
+                  type: "scatter",
+                  mode: "markers",
+                  marker: { color: "#D62728", size: 10 },
+                  name: `Roskam MTOW (${Math.floor(roskamIntersect)} lbs)`
+                },
+
+                {
+                  x: sadraey_idx,
+                  y: sadraeyIntersect,
+                  type: "scatter",
+                  mode: "markers",
+                  marker: { color: "#A57FC8", size: 10 },
+                  name: `Sadraey MTOW (${Math.floor(sadraeyIntersect)} lbs)`
+                }
+              ]}
+              layout={{
+                width: 100 + "%",
+                height: 500,
+                title:
+                  "WEIGHT SIZING CONSIDERING VARIOUS FUEL FRACTIONS  But the sizing constants are Raymer's",
+                xaxis: {
+                  title: "Wto Guess (lbs)"
+                },
+                yaxis: {
+                  title: "Wto (lbs)"
+                },
+                legend: {
+                  // yanchor: "top",
+                  // xanchor: "right"
+                }
+              }}
+            />
+          </div>
         ) : (
           ""
         )}
