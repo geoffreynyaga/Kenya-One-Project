c59d4b6eb3851a3b6b6b8c2bd638d944f0e3db77
geoffreynyaga <geoffreynyagak@gmail.com>
2020-04-11 14:03:18 +0300

    production settings

---
+++ Kenya_One/settings/production.py
@@ -0,0 +1,209 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+##################################################################################
+# File: c:\Projects\KENYA ONE PROJECT\Kenya_One\settings.py                      #
+# Project: c:\Projects\KENYA ONE PROJECT\Kenya_One                               #
+# Created Date: Thursday, January 9th 2020, 8:56:55 pm                           #
+# Author: Geoffrey Nyaga Kinyua ( <info@geoffreynyaga.com> )                     #
+# -----                                                                          #
+# Last Modified: Saturday April 11th 2020 1:50:56 pm                             #
+# Modified By:  Geoffrey Nyaga Kinyua ( <info@geoffreynyaga.com> )               #
+# -----                                                                          #
+# MIT License                                                                    #
+#                                                                                #
+# Copyright (c) 2020 KENYA ONE PROJECT                                           #
+#                                                                                #
+# Permission is hereby granted, free of charge, to any person obtaining a copy of#
+# this software and associated documentation files (the "Software"), to deal in  #
+# the Software without restriction, including without limitation the rights to   #
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies  #
+# of the Software, and to permit persons to whom the Software is furnished to do #
+# so, subject to the following conditions:                                       #
+#                                                                                #
+# The above copyright notice and this permission notice shall be included in all #
+# copies or substantial portions of the Software.                                #
+#                                                                                #
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR     #
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,       #
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE    #
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER         #
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  #
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  #
+# SOFTWARE.                                                                      #
+# -----                                                                          #
+# Copyright (c) 2020 KENYA ONE PROJECT                                           #
+##################################################################################
+
+
+import os
+
+from decouple import config  # type: ignore
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = config("SECRET_KEY")
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG: bool = True
+
+ALLOWED_HOSTS: list = [config("ALLOWED_HOSTS"),]
+
+
+# Application definition
+
+INSTALLED_APPS: list = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "rest_framework",
+    "corsheaders",
+    "accounts",
+    "initial_sizing",
+]
+
+MIDDLEWARE: list = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+ROOT_URLCONF: str = "Kenya_One.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ]
+        },
+    }
+]
+
+WSGI_APPLICATION: str = "Kenya_One.wsgi.application"
+
+
+# ======= DATABASE SECTION  ========
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
+# Uncomment the settings below if you want to use SQLITE,
+# don't forget to comment out the settings that follow
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    }
+}
+
+import dj_database_url
+
+db_from_env = dj_database_url.config()
+DATABASES["default"].update(db_from_env)
+DATABASES["default"]["CONN_MAX_AGE"] = 500
+
+
+AUTH_USER_MODEL = "accounts.User"
+
+# Password validation
+# https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
+    },
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/2.2/topics/i18n/
+
+LANGUAGE_CODE: str = "en-us"
+
+TIME_ZONE: str = "UTC"
+
+USE_I18N: bool = True
+
+USE_L10N: bool = True
+
+USE_TZ: bool = True
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ]
+        },
+    }
+]
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.2/howto/static-files/
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "staticfiles")]
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static-cdn-local")
+
+
+AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": (
+        "rest_framework.permissions.IsAuthenticatedOrReadOnly",
+        # 'rest_framework.authentication.SessionAuthentication',
+    ),
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.TokenAuthentication",
+    ),
+}
+
+CORS_ORIGIN_ALLOW_ALL: bool = False
+CORS_URLS_REGEX = r"^/api.*"
+CORS_ORIGIN_WHITELIST = (config("ALLOWED_HOSTS"),  "*.amazonaws.com")
+
+CORS_REPLACE_HTTPS_REFERER = True
+HOST_SCHEME = "https://"
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_SECONDS = 1000000
+SECURE_FRAME_DENY = True
+
+
+IS_TESTING = False
